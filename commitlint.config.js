7f134dfee093a2da2ccea87ed97d617951ad42bf
mahmudulazam <mahmudul.azam@fieldnation.com>
2025-08-17 15:57:19 +0600

    feat: release 1.0.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = { 
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Allow longer lines for semantic-release commits
+    'body-max-line-length': [0, 'always'],
+    // Ignore commits that start with chore(release):
+    'subject-case': [0, 'always'],
+  },
+  ignores: [
+    (message) => message.includes('chore(release):'),
+    (message) => message.includes('[skip ci]'),
+  ]
+};
